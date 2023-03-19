2d53f33746313b0c9903e26c509a2a80ee0bdcdf
Alex Balashov <alex.balashov@gmail.com>
2023-03-19 17:50:09 -0400

    Favicon

--- client/src/Page.js
+++ client/src/Page.js
@@ -102,6 +102,7 @@ export function Page({
                     rel="stylesheet"
                     href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css"
                 />
+                <link rel="icon" type="image/x-icon" href="/assets/favicon.ico" />
                 <SEOTag />
                 {cssBundles.map((fn, i) => (
                     <React.Fragment key={i}>
